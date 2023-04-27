14ca3fe6879e7e8e86fedbadbbb042a1398caae9
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-04-27 16:35:09 -0600

    Create new file client.js and paste the function of play.js in this new file

--- play.js
+++ play.js
@@ -1,21 +1,8 @@
-const net = require("net");
+const connect = require('./client');
+
+
 
 //establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({ //conn is an object, it represents the connection with the server, it will also handle messages from the server.
-    host: '192.168.1.72',
-    port: 50541
-  });
-
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  })
-
-  //interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();

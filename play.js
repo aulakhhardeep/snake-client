bea0909b274d1222774e961d4c73a238fe69520a
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-04-27 18:01:06 -0600

    Add comments and two functions setUpInput and handleUserInput

--- play.js
+++ play.js
@@ -1,8 +1,22 @@
+const { stdin } = require('process');
 const connect = require('./client');
 
+//setup interface to handle user input from stdin
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput); //handleUserInput is a callback, it will be triggered when key will be pressed on the keyboard.
+  return stdin; //this returned object stdin will allow to listen for keyboard input and react to it.
+};
 
-//establishes a connection with the game server
-
+const handleUserInput = function(key) {
+    if(key === '\u0003') {
+      process.exit();
+    }
+};
 
 connect();
+setupInput();
